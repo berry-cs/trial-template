d82ef5d1750db458bca21eed215822289b5533af
Nadeem Abdul Hamid <nadeem@acm.org>
2022-12-26 08:45:29 -0500

    Create SpecialTest.java

---
+++ lec020-methods/final/SpecialTest.java
@@ -0,0 +1,14 @@
+import org.junit.*;
+import static org.junit.Assert.*;
+
+public class SpecialTest
+{
+    
+	// check that small live dillos can't shelter no matter what size
+	@Test
+	public void testBabyShelter() {
+		System.out.println("Hey there!");
+    assertTrue(false);
+	}
+
+}
