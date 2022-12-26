46250ae10c0adbbaddd9111073f76260c63ed658
Nadeem Abdul Hamid <nadeem@acm.org>
2022-12-26 08:48:40 -0500

    Update SpecialTest.java

--- lec020-methods/final/SpecialTest.java
+++ lec020-methods/final/SpecialTest.java
@@ -6,9 +6,9 @@ public class SpecialTest
     
 	// check that small live dillos can't shelter no matter what size
 	@Test
-	public void testBabyShelter() {
+	public void testSomethingElse() {
 		System.out.println("Hey there!");
-    assertTrue(false);
+    assertTrue("This is going to fail on purpose!", false);
 	}
 
 }
